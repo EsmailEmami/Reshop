13aebefac3784bb2e0ed9cd9c3f8099d5e1a5841
Noname <nonamedev007@gmail.com>
2021-07-07 12:53:37 +0430

    Edit Laptop Detail

--- Reshop.Domain/Entities/Product/ProductDetail/LaptopDetail.cs
+++ Reshop.Domain/Entities/Product/ProductDetail/LaptopDetail.cs
@@ -4,69 +4,236 @@ namespace Reshop.Domain.Entities.Product.ProductDetail
 {
     public class LaptopDetail
     {
-        public LaptopDetail()
-        {
-            
-        }
-
         [Key]
         public int LaptopDetailId { get; set; }
 
-        [Display(Name = "مقدار حافظه رم (GB)")]
-        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
-        [MaxLength(10, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
-        public string RAMCapacity { get; set; }
+        //General specifications
 
-        [Display(Name = "ظرفیت حافظه داخلی")]
-        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
-        [MaxLength(50, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
-        public string InternalMemory { get; set; }
-
-        [Display(Name = "سازنده پردازنده گرافیکی")]
+        [Display(Name = "طول")]
         [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
         [MaxLength(20, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
-        public string GPUManufacturer { get; set; }
+        public string Length { get; set; }
 
-        [Display(Name = "اندازه صفحه نمایش")]
+        [Display(Name = "عرض")]
         [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
-        [MaxLength(300, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
-        public string Size { get; set; }
+        [MaxLength(20, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string Width { get; set; }
 
-        [Display(Name = "طبقه بندی")]
+        [Display(Name = "ارتفاع")]
         [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
-        [MaxLength(300, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
-        public string Category { get; set; }
+        [MaxLength(20, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string Height { get; set; }
+
+        [Display(Name = "وزن")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(20, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string Weight { get; set; }
+
+        //CPU
+
+        [Display(Name = "سازنده پردازنده")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(20, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string CpuCompany { get; set; }
 
         [Display(Name = "سری پردازنده")]
         [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
         [MaxLength(20, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
-        public string ProcessorSeries { get; set; }
+        public string CpuSeries { get; set; }
 
-        [Display(Name = "نوع حافظه RAM")]
+        [Display(Name = "مدل پردازنده")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(20, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string CpuModel { get; set; }
+
+        [Display(Name = "فرکانس پردازنده")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(20, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string CpuFerequancy { get; set; }
+
+        [Display(Name = "Cache حافظه ی ")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(20, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string CpuCache { get; set; }
+
+        //Ram
+        [Display(Name = "حافظه رم")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(3, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public int RamStorage { get; set; }
+
+        [Display(Name = "نوع حافظه رم")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(20, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string RamStorageTeachnology { get; set; }
+
+        //InternalStorage
+
+        [Display(Name = "ظرفیت حافظه ی داخل")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(5, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public int Storage { get; set; }
+
+        [Display(Name = "نوع حافظه")]
         [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
         [MaxLength(50, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
-        public string RAMType { get; set; }
+        public string StorageTeachnology { get; set; }
+
+        [Display(Name = "مشخصات حافظه ی داخلی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(100, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string StorageInformation { get; set; }
+
+        //GPU
+
+        [Display(Name = "سازنده پردازنده گرافیکی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(20, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string GpuCompany { get; set; }
+
+        [Display(Name = "مدل پردازنده گرافیکی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(100, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string GpuModel { get; set; }
+
+        [Display(Name = "  حافظه اختصاصی پردازنده گرافیکی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(5, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public int GpuRam { get; set; }
+
+        //Display
+
+        [Display(Name = "اندازه ی صفحه نمایش")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(10, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string DisplaySize { get; set; }
+
+        [Display(Name = "نوع صفحه نمایش")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(50, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string DisplayTeachnology { get; set; }
 
         [Display(Name = "دقت صفحه نمایش")]
         [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
         [MaxLength(50, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
-        public string ScreenAccuracy { get; set; }
+        public string DisplayResolutation { get; set; }
+
+        [Display(Name = "نرخ پاسخگویی صفحه")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(5, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public int RefreshDisplay { get; set; }
 
         [Display(Name = "صفحه نمایش مات")]
         [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
-        public bool IsMatteScreen { get; set; }
+        public bool BlurDisplay { get; set; }
 
         [Display(Name = "صفحه نمایش لمسی")]
         [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
-        public bool IsTouchScreen { get; set; }
+        public bool TouchDisplay { get; set; }
+
+        //Possibilities
+        [Display(Name = "درایو نوری")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        public bool DiskDrive { get; set; }
+
+        [Display(Name = "اثرانگشت")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        public bool FingerTouch { get; set; }
+
+        [Display(Name = "وبکم")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        public bool Webcam { get; set; }
+
+        [Display(Name = "کیبورد با نور پس زمینه")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        public bool BacklightKey { get; set; }
+
+        [Display(Name = "مشخصات تاچ پد")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(50, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string TouchPadInformation { get; set; }
+
+        [Display(Name = " مودم")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(50, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string ModemInformation { get; set; }
+
+        [Display(Name = "Wi-Fi")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        public bool Wifi { get; set; }
+
+        [Display(Name = "بلوتوث")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        public bool Bluetooth { get; set; }
+
+        [Display(Name = "VGA درگاه")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        public bool VgaPort { get; set; }
+
+        [Display(Name = "HTMI درگاه")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        public bool HtmiPort { get; set; }
+
+        [Display(Name = "Display درگاه")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        public bool DisplayPort { get; set; }
+
+        [Display(Name = "Lan درگاه")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        public bool LanPort { get; set; }
+
+        [Display(Name = "USB-C درگاه")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        public bool UsbCPort { get; set; }
+
+        [Display(Name = "USB-3 پشتیبانی از ")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        public bool Usb3Port { get; set; }
+
+        [Display(Name = "USB-C تعداد درگاه ")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(2, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public int UsbCQuantity { get; set; }
+
+        [Display(Name = "USB تعداد درگاه ")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(2, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public int UsbQuantity { get; set; }
+
+        [Display(Name = "USB3 تعداد درگاه ")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(2, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public int Usb3Quantity { get; set; }
+
+        //Battery
+
+        [Display(Name = " نوع باتری")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(20, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string BatteryMaterial { get; set; }
+
+        [Display(Name = " شارژدهی باتری")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(20, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string BatteryCharging{ get; set; }
+
+        [Display(Name = "توضیحات باتری ")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(20, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string BatteryInformation { get; set; }
+
+        //MoreInformation
 
         [Display(Name = "سیستم عامل")]
         [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
         [MaxLength(20, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
-        public string OperatingSystem { get; set; }
+        public string Os { get; set; }
 
-        [Display(Name = "پورت HDMI")]
+        [Display(Name = "طبقه بندی")]
         [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
-        public bool IsHDMIPort { get; set; }
+        [MaxLength(70, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string Classification { get; set; }
+
+
     }
 }
