627060d246f5748fc33f9d4b15452633cdb694f8
Noname <nonamedev007@gmail.com>
2021-07-08 12:44:27 +0430

    Edit SmartWatch Detail

--- Reshop.Domain/Entities/Product/ProductDetail/BatteryChargerDetail.cs
+++ Reshop.Domain/Entities/Product/ProductDetail/BatteryChargerDetail.cs
@@ -12,6 +12,37 @@ namespace Reshop.Domain.Entities.Product.ProductDetail
         [Key]
         public int BatteryChargerDetailId { get; set; }
 
+        [Display(Name = "طول")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(20, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string Lenght { get; set; }
+
+        [Display(Name = "عرض")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(20, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string Width { get; set; }
+
+        [Display(Name = "ارتفاع")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(20, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string Height { get; set; }
+
+        [Display(Name = "وزن")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(20, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string Weight { get; set; }
+
+        [Display(Name = "ولتاژ ورودی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(20, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string InputVoltage { get; set; }
+
+        [Display(Name = "ولتاژ خروجی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(20, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string OutputVoltage { get; set; }
+
+
         [Display(Name = "شدت جریان خروجی")]
         [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
         public double OutputCurrentIntensity { get; set; }
@@ -20,9 +51,23 @@ namespace Reshop.Domain.Entities.Product.ProductDetail
         [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
         public byte OutputPortsCount { get; set; }
 
+        [Display(Name = "نوع درگاه خروجی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(20, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string OutputTypeCharger { get; set; }
+
+        [Display(Name = "توضیحات ")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(20, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string Description { get; set; }
+
         [Display(Name = "کابل همراه")]
         [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
         [MaxLength(20, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
         public string MobileCable { get; set; }
+
+        [Display(Name = "سایر مشخصات ")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        public string MoreInformation { get; set; }
     }
 }
